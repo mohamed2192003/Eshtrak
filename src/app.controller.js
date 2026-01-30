c3778924699fa3aa01fb061044821c51c2bdea0f
Mohamed Shawky <64426063+hamo55530@users.noreply.github.com>
2026-01-30 22:10:49 +0200

    ignore cors

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,19 +1,19 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import connectDB from "./database/connection.js";
 import userRoutes from "./modules/user/user.routes.js";
 import adminRoutes from "./modules/admin/admin.routes.js";
 import { ENV } from "../config/env.service.js";
 export const bootstrap = () => {
   const app = express();
-  app.use(
-  cors({
-    origin: true, // reflect request origin (e.g. http://localhost:3000)
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-    optionsSuccessStatus: 200,
-  })
-);
+//   app.use(
+//   cors({
+//     origin: true, // reflect request origin (e.g. http://localhost:3000)
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+//     optionsSuccessStatus: 200,
+//   })
+// );
   app.use(express.json());
   connectDB();
   app.use("/api/users", userRoutes);
