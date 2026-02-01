ff71d914a1c4ba854a21291095fa1a6f4663ed93
Mohamed Shawky <64426063+hamo55530@users.noreply.github.com>
2026-02-01 02:14:34 +0200

    cors headers

--- src/app.controller.js
+++ src/app.controller.js
@@ -11,12 +11,18 @@ export const bootstrap = async() => {
       origin: "http://localhost:3000",
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-      optionsSuccessStatus: 200,
+      credentials: true,
     })
   );
-  app.options("*", cors());
+  app.use(cors(corsOptions));
   app.use(express.json());
   await connectDB();
+  app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
+  next();
+});
   app.use("/api/users", userRoutes);
   app.use("/api/admin", adminRoutes);
   app.listen(ENV.PORT, () =>
