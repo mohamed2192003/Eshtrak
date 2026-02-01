bc27a3be48b02347280515136b9bab0828f296b8
Mohamed Shawky <64426063+hamo55530@users.noreply.github.com>
2026-02-01 03:44:25 +0200

    minimal test

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,30 +1,44 @@
-import express from "express";
-import cors from "cors";
-import connectDB from "./database/connection.js";
-import userRoutes from "./modules/user/user.routes.js";
-import adminRoutes from "./modules/admin/admin.routes.js";
-import { ENV } from "../config/env.service.js";
+// import express from "express";
+// import cors from "cors";
+// import connectDB from "./database/connection.js";
+// import userRoutes from "./modules/user/user.routes.js";
+// import adminRoutes from "./modules/admin/admin.routes.js";
+// import { ENV } from "../config/env.service.js";
 
-export const bootstrap = async () => {
+// export const bootstrap = async () => {
+//   const app = express();
+
+//   const corsOptions = {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+//     credentials: true,
+//   };
+
+//   app.use(cors(corsOptions));
+//   app.use(express.json());
+
+//   await connectDB();
+//   app.get("/", (req, res) => {
+//     res.send("API ALIVE 🚀");
+//   });
+//   app.use("/api/users", userRoutes);
+//   app.use("/api/admin", adminRoutes);
+//   app.listen(process.env.PORT, "0.0.0.0", () =>
+//     console.log(`🚀 Server running on port ${process.env.PORT}`)
+//   );
+// };
+
+import express from "express";
+
+export const bootstrap = () => {
   const app = express();
 
-  const corsOptions = {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-    credentials: true,
-  };
-
-  app.use(cors(corsOptions));
-  app.use(express.json());
-
-  await connectDB();
   app.get("/", (req, res) => {
-    res.send("API ALIVE 🚀");
+    res.send("HELLO FROM RAILWAY");
   });
-  app.use("/api/users", userRoutes);
-  app.use("/api/admin", adminRoutes);
-  app.listen(process.env.PORT, "0.0.0.0", () =>
-    console.log(`🚀 Server running on port ${process.env.PORT}`)
-  );
+
+  app.listen(process.env.PORT, "0.0.0.0", () => {
+    console.log("Server started");
+  });
 };
